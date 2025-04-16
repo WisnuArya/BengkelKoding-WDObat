3c84dcdb15f2d87b180aa05338b478bcf7bfd8ab
WisnuArya <wisnu.arya.tegar.anggaraksa@gmail.com>
2025-04-16 16:49:46 +0700

    Tugas 4

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ObatController;
 
 Route::get('/', function () {
     return view('layouts.main');
@@ -8,6 +9,9 @@ Route::get('/', function () {
 Route::get('/dokter', function () {
     return view('list-dokter');
 });
-Route::get('/obat', function () {
-    return view('list-obat');
-});
+Route::get('/obat', [ObatController::class, 'index'])->name('obat.index');
+Route::get('/obat/{id}/edit', [ObatController::class, 'edit'])->name('obat.edit');
+Route::put('/obat/{id}', [ObatController::class, 'update'])->name('obat.update');
+Route::post('/obat', [ObatController::class, 'store'])->name('obat.store');
+Route::get('/obat/create', [ObatController::class, 'create'])->name('obat.create');
+Route::delete('/obat/{id}', [ObatController::class, 'destroy'])->name('obat.destroy');
